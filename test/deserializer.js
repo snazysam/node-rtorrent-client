63423cda77b22e0b0cf4e4898c5643d9fad38b01
Samantha C <snazysam@users.noreply.github.com>
2023-07-11 09:51:12 +1000

    Fix (de)serializing of buffers for loads

--- test/deserializer.js
+++ test/deserializer.js
@@ -11,9 +11,10 @@ describe("Deserializer - ", function() {
   describe( "Deserialize", function() {
 
     it( "All types", function() { 
-      const xml = "<?xml version=\"1.0\"?><methodResponse><params><param><value><string></string></value></param><param><value><string>d.custom1.set=\"bar\"</string></value></param><param><value><string><![CDATA[<special>]]></string></value></param><param><value><int>123</int></value></param><param><value><array><data><value><int>123</int></value><value><string>456</string></value></data></array></value></param><param><value><![CDATA[barfoo]]></value></param><param><value><dateTime.iso8601>1995-12-16T16:24:00.000Z</dateTime.iso8601></value></param><param><value><struct><member><name>foo</name><value><string>bar</string></value></member><member><name>bar</name><value><string>foo</string></value></member></struct></value></param></params></methodResponse></xml>";
-      let date, expected, response;
+      const xml = "<?xml version=\"1.0\"?><methodResponse><params><param><value><string></string></value></param><param><value><string>d.custom1.set=\"bar\"</string></value></param><param><value><string><![CDATA[<special>]]></string></value></param><param><value><int>123</int></value></param><param><value><array><data><value><int>123</int></value><value><string>456</string></value></data></array></value></param><param><value><base64>YmFyZm9v</base64></value></param><param><value><dateTime.iso8601>1995-12-16T16:24:00.000Z</dateTime.iso8601></value></param><param><value><struct><member><name>foo</name><value><string>bar</string></value></member><member><name>bar</name><value><string>foo</string></value></member></struct></value></param></params></methodResponse></xml>";
+      let buf, date, expected, response;
 
+      buf = Buffer.from( "barfoo" );
       date = new Date( "December 17, 1995 03:24:00" );
       expected = [
         '',
@@ -21,7 +22,7 @@ describe("Deserializer - ", function() {
         '<special>',
         123,
         [ 123, "456" ],
-        "barfoo",
+        buf,
         date,
         { foo: "bar", bar: "foo" }
       ];
