027eb91df2fddb2344dae98008d540d193b73f84
Samantha C <snazysam@users.noreply.github.com>
2026-01-15 19:22:31 +1100

    Fix test case localisation

--- test/serializer.spec.ts
+++ test/serializer.spec.ts
@@ -10,7 +10,7 @@ describe("Serializer - ", function() {
   describe( "Serialize", function() {
 
     it( "All types", function() {
-      const expected = "<?xml version=\"1.0\"?><methodCall><methodName>d.foobar</methodName><params><param><value><string></string></value></param><param><value><string>d.custom1.set=\"bar\"</string></value></param><param><value><string><![CDATA[<special>]]></string></value></param><param><value><int>123</int></value></param><param><value><array><data><value><int>123</int></value><value><string>456</string></value></data></array></value></param><param><value><base64>YmFyZm9v</base64></value></param><param><value><dateTime.iso8601>1995-12-16T16:24:00.000Z</dateTime.iso8601></value></param><param><value><struct><member><name>foo</name><value><string>bar</string></value></member><member><name>bar</name><value><string>foo</string></value></member></struct></value></param></params></methodCall>";
+      const expected = "<?xml version=\"1.0\"?><methodCall><methodName>d.foobar</methodName><params><param><value><string></string></value></param><param><value><string>d.custom1.set=\"bar\"</string></value></param><param><value><string><![CDATA[<special>]]></string></value></param><param><value><int>123</int></value></param><param><value><array><data><value><int>123</int></value><value><string>456</string></value></data></array></value></param><param><value><base64>YmFyZm9v</base64></value></param><param><value><dateTime.iso8601>1995-12-17T03:24:00.000Z</dateTime.iso8601></value></param><param><value><struct><member><name>foo</name><value><string>bar</string></value></member><member><name>bar</name><value><string>foo</string></value></member></struct></value></param></params></methodCall>";
       let xml: string='',
       buf: Buffer,
       date: Date,
@@ -18,7 +18,7 @@ describe("Serializer - ", function() {
       params: Array<any>;
 
       buf = Buffer.from( "barfoo" );
-      date = new Date( "December 17, 1995 03:24:00" );
+      date = new Date( "1995-12-17T03:24:00Z" );
 
       method = "d.foobar";
       params = [
